c5b757c2896c4544066506e058eab534368fe5a7
Sandeep Rasali <S342931@students.cdu.edu.au>
2021-10-14 02:58:31 +0930

    Update graphics.py

--- graphics/graphics.py
+++ graphics/graphics.py
@@ -4,44 +4,63 @@ from typing import Sized
 t = trt.Turtle()
 
 t.pu()
-t.goto(-100, -300)
+t.goto(530, -295)
 t.pd()
-t.lt(90)
-t.fd(200)
-t.pu()
-t.goto(100, -300)
-t.pd()
-t.fd(200)
-t.pu()
-t.goto(-100, -300)
-t.seth(0)
-t.pd()
-t.fd(50)
-t.lt(90)
-
-i = 1
-while i < 16:
-    t.fd(i+6)
-    t.seth(90 - (i/2))
-    i +=2
-    
-t.pu()
-t.goto(100, -300)
+t.width(4)
 t.seth(180)
-t.pd()
-t.fd(50)
-t.rt(90)
+t.fd(120)
+t.seth(200)
+t.fd(10)
+t.seth(180)
+t.fd(344)
 
 i = 1
 while i < 16:
+    t.width(5)
     t.fd(i+6)
     t.seth(90 + (i/2))
     i +=2
 
+t.width(5)
 t.circle(43, extent=170)
-t.hideturtle()
+# t.hideturtle()
 
-    
+t.pu()
+t.goto(-530, -295)
+t.pd()
+t.seth(0)
+t.width(4)
+t.fd(120)
+t.seth(335)
+t.fd(10)
+t.seth(0)
+t.fd(344)
+
+
+i = 1
+while i < 16:
+    t.width(5)
+    t.fd(i+6)
+    t.seth(90 - (i/2))
+    i +=2
+     
+
+
+print(t.position())  
+
+t.pu()
+t.goto(-100, -296)
+t.speed(1)
+t.width(2)
+t.seth(90)
+t.pd()
+t.fd(200)
+t.seth(150)
+t.fd(7)
+t.seth(90)
+t.fd(30)
+
+
 
 trt.done()
 
